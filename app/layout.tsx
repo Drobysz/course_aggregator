02477d18becb0db7b27822c844990495cc4136aa
Drobysz <alexanderdrobyschevski@gmail.com>
2025-02-19 10:54:06 +0100

    Add some css variable and Add Noto Sans font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,15 @@
 import "./globals.css";
 
+import { Noto_Sans } from 'next/font/google'
+
+const notoSans = Noto_Sans({
+    subsets: ['latin'],
+    display: 'swap',
+  })
 
 export default function RootLayout({children}: Readonly<{children: React.ReactNode}>) {
   return (
-    <html lang="ru">
+    <html lang="ru" className={notoSans.className}> 
       <body>
         {children}
       </body>
