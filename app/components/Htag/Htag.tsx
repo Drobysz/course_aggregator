4afaf475b4f7c85ea9d0ce94cdb4a6fa1a0fc63f
Drobysz <alexanderdrobyschevski@gmail.com>
2025-02-19 13:16:02 +0100

    Add Htag component

---
+++ app/components/Htag/Htag.tsx
@@ -0,0 +1,18 @@
+import Htagprops from "./Htag.props";
+import styles from './Htag.module.scss'
+import { JSX } from "react";
+
+export default function Htag ({tag, children}: Htagprops): JSX.Element {
+    console.log(styles.h1)
+    console.log(styles.h2)
+    console.log(styles.h3)
+    return <h3
+             className={
+                tag === 'h1' ? styles.h1:
+                tag === 'h2' ? styles.h2:
+                tag === 'h3' ? styles.h3: ''
+            }
+            >
+                {children}
+            </h3>
+}
