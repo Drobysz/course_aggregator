3fe80500894f0395041d23e07365b818c56ee670
Drobysz <alexanderdrobyschevski@gmail.com>
2025-02-22 10:12:35 +0100

    Add "arrow" prop to Btn.props.ts

--- app/components/Button/Btn.props.ts
+++ app/components/Button/Btn.props.ts
@@ -1,6 +1,7 @@
-import { ReactNode } from "react";
+import { ButtonHTMLAttributes, DetailedHTMLProps, ReactNode } from "react";
 
-export default interface Htagprops {
+export default interface BtnProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
     children: ReactNode;
     appearence: 'primary' | 'ghost';
+    arrow?: 'right' | 'down' | 'none'
 }
