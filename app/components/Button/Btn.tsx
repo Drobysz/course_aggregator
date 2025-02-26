4b6aaf5d2226719b846f3c6e54018efd44c2a594
Drobysz <alexanderdrobyschevski@gmail.com>
2025-02-26 20:00:31 +0100

    Add animated svg of Arrow to Button component
    * An additional condition for the Button component defining the presence of an animated arrow inside of the Button component

--- app/components/Button/Btn.tsx
+++ app/components/Button/Btn.tsx
@@ -2,16 +2,34 @@ import BtnProps from './Btn.props'
 import styles from './Btn.module.scss'
 import { JSX } from "react"
 import cn from 'classnames'
+import Image from 'next/image'
 
-export function Btn ({appearence, children}: BtnProps): JSX.Element{
+export function Btn ({appearence, arrow='none', children, className, ...props}: BtnProps): JSX.Element{
     return (
     <button
-        className={cn(styles.btn, {
+        className={cn(styles.btn, className, {
             [styles.primary]: appearence == 'primary',
             [styles.ghost]: appearence == 'ghost'
         })}
+        {...props}
     >
         {children}
+        {
+         arrow !== 'none'
+             && <Image
+                  
+                    className={
+                        cn(styles.arrow, {
+                            [styles.down]: arrow = 'down',
+                            [styles.right]: arrow = 'right'
+                        })                
+                    }
+                    src='Arrow.svg'
+                    alt='#'
+                    width={6}
+                    height={10}
+                />
+        }
     </button>
 )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <div>
       <Htag tag="h1">Text</Htag>
-      <Btn appearence='ghost'>Learn more</Btn>
+      <Btn appearence='primary' arrow='none'>Start</Btn>
+      <Btn appearence='ghost' arrow='down'>Learn more</Btn>
     </div>
   );
 }
