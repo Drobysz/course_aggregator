a2e0cbfbf062fe8e94600bdad2de27a2b03160b3
Drobysz <alexanderdrobyschevski@gmail.com>
2025-02-27 17:42:18 +0100

    Modify Arrow.svg icon in Button component
    * Make arrow icon (ArrowIcon.tsx) as an separated component due to be able to change its parameters
    
    * SCSS
    
     -Apply items-center property in order to align elements inside of the button

--- app/components/Button/Btn.tsx
+++ app/components/Button/Btn.tsx
@@ -2,7 +2,7 @@ import BtnProps from './Btn.props'
 import styles from './Btn.module.scss'
 import { JSX } from "react"
 import cn from 'classnames'
-import Image from 'next/image'
+import ArrowIcon from './ArrowIcon'
 
 export function Btn ({appearence, arrow='none', children, className, ...props}: BtnProps): JSX.Element{
     return (
@@ -16,18 +16,13 @@ export function Btn ({appearence, arrow='none', children, className, ...props}: 
         {children}
         {
          arrow !== 'none'
-             && <Image
-                  
+             && <ArrowIcon
                     className={
                         cn(styles.arrow, {
-                            [styles.down]: arrow = 'down',
-                            [styles.right]: arrow = 'right'
+                            [styles.down]: arrow === 'down',
+                            [styles.right]: arrow === 'right'
                         })                
                     }
-                    src='Arrow.svg'
-                    alt='#'
-                    width={6}
-                    height={10}
                 />
         }
     </button>
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <Htag tag="h1">Text</Htag>
-      <Btn appearence='primary' arrow='none'>Start</Btn>
+      <Btn appearence='primary' arrow='right'>Start</Btn>
       <Btn appearence='ghost' arrow='down'>Learn more</Btn>
     </div>
   );
