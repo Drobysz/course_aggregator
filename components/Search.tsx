3174660b2878483f70893b330cbba0f64c7cea3b
Drobysz <alexanderdrobyschevski@gmail.com>
2025-04-01 08:42:15 +0200

    Add 2 icons to the public directory

---
+++ components/Search.tsx
@@ -0,0 +1,45 @@
+// Props
+import { FC, DetailedHTMLProps, HTMLAttributes, useState } from "react"
+
+// Dependencies
+import cn from 'classnames'
+import { useRouter } from "next/navigation";
+
+// Components
+import { Input, Btn } from "./index";
+import Image from "next/image";
+
+interface SearchProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>{
+    className?: string,
+};
+
+export const Search: FC<SearchProps> = ({ className='none', ...props }) => {
+    const [search, setSearch] = useState<string>('');
+    const router = useRouter();
+
+    const pushTo = ()=> {    
+        router.push(`/search?q=${search}`);
+    };
+    const handleKeyDown = (e: KeyboardEvent)=> {
+        if (e.key == 'Enter') pushTo();
+    };
+
+    return (
+        <div className={ cn(className, 'relative') } {...props}>
+            <Input 
+                placeholder="search..."
+                value={search}
+                onChange={ (e)=> setSearch(e.target.value) }
+                onKeyDown={()=> handleKeyDown}
+            />
+
+            <Btn appearence="primary" 
+                onClick={()=> pushTo()}
+                className="absolute top-0.5 right-0.5"
+            >
+                <Image src="/Loop.svg" alt="#" width="15" height="15"/>
+            </Btn>
+        </div>
+    );
+};
+
