d5ced2e24ba1dda5a75e13e16dcbd79c818a03c9
Drobysz <alexanderdrobyschevski@gmail.com>
2025-04-05 11:00:55 +0200

    Change Logo in SideBar

--- app/layout/Sidebar.tsx
+++ app/layout/Sidebar.tsx
@@ -3,17 +3,18 @@ import { Menu } from "./Menu/Menu";
 import { Search } from '@/components/index'
 import cn from 'classnames'
 
-// Logo
-import Logo from './Logo'
+// Image
+import Image from "next/image";
 
 interface SidebarProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>{ }
 
-const sideBarStyle: string = "grid content-start gap-5 pt-[2.125rem]";
+const sideBarStyle: string = "grid content-start gap-5 pt-[1.125rem]";
 
 export const Sidebar: FC<SidebarProps> = ({className, ...props}) => {
     return (
         <div className={ cn(className, sideBarStyle) } {...props}>
-            <Logo className="justify-self-center"/>
+            <Image src="/skill-aggregator-logo.png" alt="Logo" width="159" height="44" className="justify-self-center"/>
+            {/* <Logo className="justify-self-center"/> */}
             <Search className="justify-self-end"/>
             <Menu/>
         </div>
