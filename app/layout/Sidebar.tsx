f21f3f1e9a5b4badb68c1898958abe654197c9d0
Drobysz <alexanderdrobyschevski@gmail.com>
2025-04-05 11:29:00 +0200

    * Update imports for Sidebar * Classify imports for SideBar * Wrap tag Image (Logo) into tag Link   - tag Link leads to the main page

--- app/layout/Sidebar.tsx
+++ app/layout/Sidebar.tsx
@@ -1,6 +1,12 @@
+// Props
 import { DetailedHTMLProps, HTMLAttributes, FC } from "react";
+
+// Components
 import { Menu } from "./Menu/Menu";
 import { Search } from '@/components/index'
+import Link from "next/link";
+
+// Dependencies
 import cn from 'classnames'
 
 // Image
@@ -13,7 +19,10 @@ const sideBarStyle: string = "grid content-start gap-5 pt-[1.125rem]";
 export const Sidebar: FC<SidebarProps> = ({className, ...props}) => {
     return (
         <div className={ cn(className, sideBarStyle) } {...props}>
-            <Image src="/skill-aggregator-logo.png" alt="Logo" width="159" height="44" className="justify-self-center rounded-2xl"/>
+            <Link className="cursor-pointer" href="/">
+                <Image src="/skill-aggregator-logo.png" alt="Logo" width="159" height="44" className="justify-self-center rounded-2xl"/>
+            </Link>
+            
             {/* <Logo className="justify-self-center"/> */}
             <Search className="justify-self-end"/>
             <Menu/>
